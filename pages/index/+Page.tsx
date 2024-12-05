5299999d721fbfc3112d043691188c5b06f8dfbd
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-06 00:22:47 +0100

    feat(ui): add Pokémon search functionality to Pokédex page
    - Added a `filteredData` state to manage the filtered list of Pokémon.
    - Integrated the `SearchPokemon` function to perform server-side filtering based on the search term.
    - Updated the input field to trigger the search on user input (`onChange`).
    - Conditionally rendered the `Gallery` component based on the filtered Pokémon list or the full list.
    - Improved user experience by allowing real-time Pokémon searches within the Pokédex.
    - Logged the filtered data for debugging purposes.

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -2,13 +2,32 @@ import React from "react";
 import type { Data } from "./+data";
 import { useData } from "vike-react/useData";
 import { Gallery } from "../../components/Gallery";
+import type { Pokemon } from "./types";
+import { useState } from "react";
+import { SearchPokemon } from "./SearchPokemon.telefunc";
 
 export default function Page() {
   const data = useData<Data>();
+  const [filteredData, setFilteredData] = useState<Pokemon[]>(data.pokemonList);
+  
+  const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filteredData = await SearchPokemon(searchTerm);
+    setFilteredData(filteredData);
+  }
+
+  console.log(filteredData)
+
+
   return (
     <div>
       <h1>Pokédex</h1>
-      <Gallery data={data} />
+      <input type="text" placeholder="Rechercher un Pokémon" onChange={handleSearch}/>
+      {filteredData.length > 0 ? (
+        <Gallery data={filteredData} />
+      ) : (
+        <Gallery data={data} />
+      )}
     </div>
   );
 }
