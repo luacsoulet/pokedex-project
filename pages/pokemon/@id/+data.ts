d4d627aa2ccd548200001399edfc6f0b8fba86a8
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-05 11:09:14 +0100

    feat(api): add server-side data fetching for Pokémon details with environment variables
    Implemented a server-side data fetching function to retrieve Pokémon details using API configuration from environment variables.
    
    - Added a `data` function to fetch Pokémon details based on the `id` in the route parameters.
    - Utilized environment variables (`VITE_POKEMON_API` and `VITE_POKEMON_API_KEY`) to configure the API endpoint and authorization headers.
    - Included error handling for missing API URL in environment variables.
    - Improved modularity and reusability by leveraging the `PageContextServer` and `ResearchPokemon` types.
    - Returned the fetched Pokémon data as `currentPokemon` for seamless integration with the application.

---
+++ pages/pokemon/@id/+data.ts
@@ -0,0 +1,24 @@
+import type { PageContextServer } from "vike/types";
+import type { ResearchPokemon } from "../../index/types";
+export type Data = Awaited<ReturnType<typeof data>>;
+
+export default async function data(pageContext: PageContextServer) {
+
+  if (!import.meta.env.VITE_POKEMON_API) {
+    throw new Error('POKEMON_API URL is not defined in environment variables');
+  }
+
+  const headers: HeadersInit = {};
+  if (import.meta.env.VITE_POKEMON_API_KEY) {
+    headers.Authorization = `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`;
+  }
+
+  const pokemon = await fetch(`${import.meta.env.VITE_POKEMON_API}/${pageContext.routeParams.id}`, {
+    headers,
+  })
+  .then((res) => res.json());
+
+  return {
+    currentPokemon: pokemon,
+  };
+}
