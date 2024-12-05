ab1e8fc844301b4cceeacc547d5ab83da8e98e8d
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-05 14:45:26 +0100

    feat(api): fetch Pokémon team details based on team state
    - Added `getTeam` function to asynchronously fetch Pokémon details based on an array of `PokemonId`.
    - Used `Promise.all` to perform parallel API calls for efficient data retrieval.
    - Configured API requests using environment variables (`VITE_POKEMON_API` and `VITE_POKEMON_API_KEY`) for endpoint and authorization.
    - Implemented error handling for missing API configuration.
    - Returned a structured object containing the fetched team data for use in the application.

---
+++ pages/team/TeamList.telefunc.ts
@@ -0,0 +1,22 @@
+import type { PokemonId } from "../index/types";
+
+export const getTeam = async (teamState: PokemonId[]) => {
+  const team = await Promise.all(teamState.map(async (pokemon: PokemonId) => {
+    if (!import.meta.env.VITE_POKEMON_API) {
+      throw new Error('POKEMON_API URL is not defined in environment variables');
+    }
+
+    const headers: HeadersInit = {};
+    if (import.meta.env.VITE_POKEMON_API_KEY) {
+      headers.Authorization = `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`;
+    }
+
+    const actualPokemon = await fetch(`${import.meta.env.VITE_POKEMON_API}/${pokemon.slug}`, {
+      headers,
+    }).then((res) => res.json());
+
+    return actualPokemon.current;
+  }));
+
+  return { team };
+}
