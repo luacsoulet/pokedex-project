a1df4656632439de589fdb2b2e63a9b83b8fb919
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-05 10:23:59 +0100

    feat: add server-side data fetching for Pokemon APIv
    - Implemented an async function `data` to fetch Pokemon data from the API.
    - Validates the presence of `VITE_POKEMON_API` environment variable and throws an error if undefined.
    - Added support for an optional API key using the `VITE_POKEMON_API_KEY` environment variable.
    - Fetches the Pokemon list and returns it as part of the response.
    - Ensures secure and configurable API access with dynamic headers.

---
+++ pages/index/+data.tsx
@@ -0,0 +1,24 @@
+import type { PageContextServer } from "vike/types";
+import type { PokemonList } from "./types";
+export type Data = Awaited<ReturnType<typeof data>>;
+
+export default async function data(pageContext: PageContextServer) {
+
+  if (!import.meta.env.VITE_POKEMON_API) {
+    throw new Error('POKEMON_API URL is not defined in environment variables');
+  }
+
+  const headers: HeadersInit = {};
+  if (import.meta.env.VITE_POKEMON_API_KEY) {
+    headers.Authorization = `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`;
+  }
+
+  const pokemonList = await fetch(`${import.meta.env.VITE_POKEMON_API}`, {
+      headers,
+    })
+    .then((res) => res.json());
+
+  return {
+    pokemonList,
+  };
+}
