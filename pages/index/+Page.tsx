3ab007d48341622731df35872b7722be41e0ad9b
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-06 11:45:17 +0100

    feat(ui): enhance Pokédex page with type filtering and loading state
    - Introduced a type dropdown to filter Pokémon based on their types using `getTypes` from the server.
    - Added a `selectedType` state to manage the current selected type for filtering.
    - Implemented dynamic filtering logic to update Pokémon list based on the selected type.
    - Integrated a `resetFilters` function to clear all applied filters and reset the Pokémon list.
    - Added a loading state (`isLoading`) to provide feedback during search and filtering operations.
    - Enhanced the search functionality by combining type filtering and text-based search.
    - Updated the `Gallery` component usage to include the filtered Pokémon list and loading state.
    - Improved user experience by providing a streamlined and dynamic way to filter Pokémon.

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -3,37 +3,65 @@ import type { Data } from "./+data";
 import { useData } from "vike-react/useData";
 import { Gallery } from "../../components/Gallery";
 import type { Pokemon } from "./types";
-import { useState } from "react";
-import { SearchPokemon } from "./SearchPokemon.telefunc";
+import { useState, useEffect } from "react";
+import { SearchPokemon, getTypes } from "./SearchPokemon.telefunc";
+import type { Type, Types } from "./types";
 
 export default function Page() {
   const data = useData<Data>();
   const [filteredData, setFilteredData] = useState<Pokemon[]>(data.pokemonList);
-  
+  const [types, setTypes] = useState<Type[]>([]);
+  const [selectedType, setSelectedType] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsLoading(true);
     const searchTerm = e.target.value.toLowerCase();
     const filteredData = await SearchPokemon(searchTerm);
     setFilteredData(filteredData);
+    setIsLoading(false);
   }
 
-  const types: string[] = [...new Set(data.pokemonList.map((pokemon: Pokemon) => pokemon.types?.[0].name))];
+  const handleTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedType(e.target.value);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (selectedType && selectedType !== 'Select a type') {
+      setFilteredData((filteredData?.length ? filteredData : data.pokemonList).filter((pokemon: Pokemon) => 
+        pokemon.types?.[0].slug === selectedType
+      ));
+    } else {
+      setFilteredData(filteredData || data.pokemonList);
+    }
+    setIsLoading(false)
+  }, [selectedType, data.pokemonList]);
+
+  useEffect(() => {
+    getTypes().then((types: Types) => setTypes(types));
+  }, []);
+  console.log(types);
+
+  const resetFilters = () => {
+    setSelectedType('');
+    setFilteredData(data.pokemonList);
+  }
 
   return (
     <div>
       <h1>Pokédex</h1>
-      <input type="text" placeholder="Rechercher un Pokémon" onChange={handleSearch}/>
-      {filteredData.length > 0 ? (
-        <Gallery data={filteredData} />
-      ) : (
-        <Gallery data={data} />
-      )}
       <div>
-        <select name="type" id="type">
-          {types.map((type: string, index: number) => (
-            <option key={index} value={type}>{type}</option>
+        <input type="text" placeholder="Rechercher un Pokémon" onChange={handleSearch}/>
+        <select name="type" id="type" onChange={handleTypeChange}>
+          <option value="Select a type">Select a type</option>
+          {Array.isArray(types) && types.map((type: Type, index: number) => (
+            <option key={index} value={type.slug}>{type.name}</option>
           ))}
         </select>
+        <button onClick={resetFilters}>Reset filters</button>
       </div>
+      <Gallery data={filteredData} isLoading={isLoading} />
     </div>
   );
 }
