b6893605a2577a5eb36ca9fcd3a59bd4122bf1c1
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-06 14:23:50 +0100

    feat(api): add fetchMorePokemon function and refine existing API calls
    - Added a new `fetchMorePokemon` function to enable fetching additional Pokémon data with `offset` and `pageSize` parameters.
    - Updated `SearchPokemon` to use a default limit of 30 for more efficient search results.

--- pages/index/SearchPokemon.telefunc.ts
+++ pages/index/SearchPokemon.telefunc.ts
@@ -1,5 +1,5 @@
 export async function SearchPokemon(searchTerm: string) {
-  const response = await fetch(`${import.meta.env.VITE_POKEMON_API}/pokemon?limit=60&with=types&search=${searchTerm}`, {
+  const response = await fetch(`${import.meta.env.VITE_POKEMON_API}/pokemon?limit=30&with=types&search=${searchTerm}`, {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`
     }
@@ -20,6 +20,19 @@ export async function getTypes() {
       Authorization: `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`
     }
   });
+  const data = await response.json();
+  return data;
+}
+
+export async function fetchMorePokemon(offset: number, pageSize: number) {
+  const response = await fetch(
+    `${import.meta.env.VITE_POKEMON_API}/pokemon?limit=${pageSize}&offset=${offset}&with=types`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`
+      }
+    }
+  );
   const data = await response.json();
   return data;
 }
