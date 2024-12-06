e6e7911132ab3e1333e4fca4510fdd524cf49034
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-06 11:50:22 +0100

    feat(api): enhance Pokémon search functions with pagination and type fetching
    - Updated `SearchPokemon` to include `limit=60` in the query for controlled pagination and ensure consistent data fetching.
    - Modified `SearchPokemonByType` to use the updated API endpoint with the `/pokemon` route for type-based filtering.
    - Added a new `getTypes` function to fetch all available Pokémon types from the `/type` endpoint.
    - Standardized the use of `Authorization` headers across all API calls for secure access.
    - Enhanced functionality to provide more structured and robust data retrieval.

--- pages/index/SearchPokemon.telefunc.ts
+++ pages/index/SearchPokemon.telefunc.ts
@@ -1,5 +1,5 @@
 export async function SearchPokemon(searchTerm: string) {
-  const response = await fetch(`${import.meta.env.VITE_POKEMON_API}?with=types&search=${searchTerm}`, {
+  const response = await fetch(`${import.meta.env.VITE_POKEMON_API}/pokemon?limit=60&with=types&search=${searchTerm}`, {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`
     }
@@ -9,7 +9,17 @@ export async function SearchPokemon(searchTerm: string) {
 };
 
 export async function SearchPokemonByType(type: string) {
-  const response = await fetch(`${import.meta.env.VITE_POKEMON_API}?type=${type}`);
+  const response = await fetch(`${import.meta.env.VITE_POKEMON_API}/pokemon?type=${type}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getTypes() {
+  const response = await fetch(`${import.meta.env.VITE_POKEMON_API}/type`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_POKEMON_API_KEY}`
+    }
+  });
   const data = await response.json();
   return data;
 }
