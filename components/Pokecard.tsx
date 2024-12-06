5c0fb81b1e378cbbdd621e9f6f3c33f9cdbb23e9
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-06 16:39:02 +0100

    feat(ui): add delete button to Pokecard component
    Enhanced the `Pokecard` component by adding an optional delete button

--- components/Pokecard.tsx
+++ components/Pokecard.tsx
@@ -1,6 +1,7 @@
 type PokecardProps = {
   name: string;
   image: string | null;
+  onDelete?: () => void;
 }
 
 export const Pokecard = (props: PokecardProps) => {
@@ -8,6 +9,7 @@ export const Pokecard = (props: PokecardProps) => {
     <div>
       {props.image ? <img src={props.image} alt={props.name} /> : <p>Image non disponible</p>}
       <h2>{props.name}</h2>
+      {props.onDelete && <button onClick={props.onDelete}>Delete</button>}
     </div>
   );
 };
