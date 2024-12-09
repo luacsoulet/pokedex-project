8220199b3d7975d4a828d0aa1333e15cb81c24b3
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-12-09 21:19:34 +0100

    delete vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { telefunc } from "telefunc/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
-import ViteVercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [vike({}), react({}), telefunc(), ViteVercel()],
+  plugins: [vike({}), react({}), telefunc()],
 });
